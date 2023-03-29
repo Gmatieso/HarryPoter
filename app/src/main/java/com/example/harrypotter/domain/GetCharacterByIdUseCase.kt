fe2478b38fc9634dbe0256595b5005d2260dd487
Gmatieso <matiesogeoffrey@gmail.com>
2023-03-29 14:32:13 +0300

    GetCharacterByIdUseCase Done

---
+++ app/src/main/java/com/example/harrypotter/domain/GetCharacterByIdUseCase.kt
@@ -0,0 +1,12 @@
+package com.example.harrypotter.domain
+
+import com.example.harrypotter.domain.items.CharacterItem
+import com.example.harrypotter.domain.items.SpecificCharacterItem
+import com.example.harrypotter.repo.HarryPotterRepository
+import javax.inject.Inject
+
+class GetCharacterByIdUseCase @Inject constructor(private val harryPotterRepository: HarryPotterRepository) {
+    suspend operator fun invoke(house: String): SpecificCharacterItem {
+        return harryPotterRepository.getCharacterById(house)
+    }
+}
