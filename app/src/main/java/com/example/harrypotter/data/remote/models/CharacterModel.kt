b860a0493bf825bece76028103e02b68999513e5
Gmatieso <matiesogeoffrey@gmail.com>
2023-03-30 02:15:22 +0300

    Code Refactored CharacterModel and Spell Model

--- app/src/main/java/com/example/harrypotter/data/remote/models/CharacterModel.kt
+++ app/src/main/java/com/example/harrypotter/data/remote/models/CharacterModel.kt
@@ -1,14 +1,24 @@
 package com.example.harrypotter.data.remote.models
 
 data class CharacterModel(
-    val image: String,
-    val name: String,
-    val species: String,
-    val gender: String,
+    val actor: String,
+    val alive: Boolean,
+    val alternate_actors: List<String>,
+    val alternate_names: List<String>,
     val ancestry: String,
-    val house: String,
     val dateOfBirth: String,
     val eyeColour: String,
+    val gender: String,
     val hairColour: String,
+    val hogwartsStaff: Boolean,
+    val hogwartsStudent: Boolean,
+    val house: String,
+    val id: String,
+    val image: String,
+    val name: String,
+    val patronus: String,
+    val species: String,
+    val wand: Wand,
     val wizard: Boolean,
+    val yearOfBirth: Int
 )
--- app/src/main/java/com/example/harrypotter/data/remote/models/Spell.kt
+++ app/src/main/java/com/example/harrypotter/data/remote/models/Spell.kt
@@ -1,6 +1,6 @@
 package com.example.harrypotter.data.remote.models
 
-data class SpellItem(
+data class Spell(
     val description: String,
     val id: String,
     val name: String
