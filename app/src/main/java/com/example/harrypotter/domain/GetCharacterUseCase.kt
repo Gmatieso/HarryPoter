e213f5540ac83b746c6f56f17adf3fd6021a5c4a
Gmatieso <matiesogeoffrey@gmail.com>
2023-03-29 14:25:24 +0300

    GetCharacterUseCase Done

---
+++ app/src/main/java/com/example/harrypotter/domain/GetCharacterUseCase.kt
@@ -0,0 +1,13 @@
+package com.example.harrypotter.domain
+
+import com.example.harrypotter.domain.items.CharacterItem
+import com.example.harrypotter.repo.HarryPotterRepository
+import javax.inject.Inject
+
+class GetCharacterUseCase @Inject constructor(private val harryPotterRepository: HarryPotterRepository) {
+
+    //Defines use case for retrieving a list of CharacterItem objects from a datasource and shuffle their order
+    suspend operator fun invoke(): List<CharacterItem> {
+        return harryPotterRepository.getCharacters().shuffled()
+    }
+}
