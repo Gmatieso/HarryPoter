54abf3a0239c0b077635ce60a86a6163719e0168
Gmatieso <matiesogeoffrey@gmail.com>
2023-03-31 13:07:42 +0300

    Navigation Done

---
+++ app/src/main/java/com/example/harrypotter/ui/nav/Navigation.kt
@@ -0,0 +1,33 @@
+package com.example.harrypotter.ui.nav
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.harrypotter.ui.vm.HarryPotterViewModel
+import com.example.harrypotter.util.Constants.Companion.KEY_HOUSE_ID
+import com.example.harrypotter.util.Constants.Screens.DETAIL_SCREEN
+import com.example.harrypotter.util.Constants.Screens.HOME_SCREEN
+
+sealed class Screens (val route: String) {
+    object  Home: Screens(route = HOME_SCREEN)
+    object  Details: Screens(route = DETAIL_SCREEN)
+}
+
+//Setting up NavHost
+@Composable
+fun SetupNavHost(navController: NavHostController, harryPotterViewModel: HarryPotterViewModel) {
+
+    NavHost(navController = navController, startDestination = Screens.Home.route) {
+
+        composable(route = Screens.Home.route) {
+            HomeScreen(harryPotterViewModel = harryPotterViewModel , navController = navController)
+        }
+
+        composable(route = Screens.Details.route + "/{$KEY_HOUSE_ID}") { backStackEntry ->
+            DetailScreen(id = backStackEntry.arguments?.getString(KEY_HOUSE_ID)?: "1", harryPotterViewModel = harryPotterViewModel , navController = navController)
+        }
+
+    }
+
+}
