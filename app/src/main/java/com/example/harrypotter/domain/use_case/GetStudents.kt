7d3f96fb986aa4bad5858016a005080b979eb0d1
Gmatieso <matiesogeoffrey@gmail.com>
2023-04-05 00:00:21 +0300

    created GetStudent usecase

---
+++ app/src/main/java/com/example/harrypotter/domain/use_case/GetStudents.kt
@@ -0,0 +1,9 @@
+package com.example.harrypotter.domain.use_case
+
+import com.kenstarry.harrypotter.core.data.HarryPotterApi
+
+class GetStudents(
+    private val harryPotterApi: HarryPotterApi
+) {
+    suspend operator fun invoke() = harryPotterApi.getStudents()
+}
