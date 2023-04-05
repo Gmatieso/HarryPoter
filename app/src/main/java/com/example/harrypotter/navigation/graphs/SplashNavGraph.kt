94a743078913db951beeb3eb57cf6af1b0701d3c
Gmatieso <matiesogeoffrey@gmail.com>
2023-04-05 10:41:07 +0300

    SplashNavGraph Done

--- app/src/main/java/com/example/harrypotter/navigation/graphs/SplashNavGraph.kt
+++ app/src/main/java/com/example/harrypotter/navigation/graphs/SplashNavGraph.kt
@@ -1,12 +1,13 @@
-package com.kenstarry.harrypotter.navigation.graphs
+package com.example.harrypotter.navigation.graphs
 
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
-import com.kenstarry.harrypotter.feature_splash_screen.presentation.SplashScreen
-import com.kenstarry.harrypotter.navigation.NavConstants
-import com.kenstarry.harrypotter.navigation.screens.Screens
+import com.example.harrypotter.navigation.NavConstants
+import com.example.harrypotter.navigation.screens.Screens
+import com.example.harrypotter.presentation.SplashScreen
+
 
 fun NavGraphBuilder.splashNavGraph(
     navHostController: NavHostController
